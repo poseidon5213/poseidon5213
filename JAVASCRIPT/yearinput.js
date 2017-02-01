271a0cb2ee89484a88bec9ee92de19ee7e61e7b6
Tj <Tajairi_Neuson@davinci.local>
2017-02-01 13:54:59 -0800

    Website

---
+++ JAVASCRIPT/yearinput.js
@@ -0,0 +1,5 @@
+var YearInfo = document.getElementById('YearInfo').innerHTML;
+var YearSubmission = document.getElementById('YearInput').elements.value;
+
+console.log(YearInfo);
+console.log(YearSubmission);
