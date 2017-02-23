713f9347b2e04a5a232ffc4e76781602855058d9
Tajairi Neuson <tajairineuson@gmail.com>
2017-02-23 14:02:19 -0800

    New Keyboard Functionality Removed Keyboards
    Testing New Keyboard Navigation and removed HTCaccess because I
    discovered Github Pages does not allow that. Also truly removed
    index.html overflow this time

---
+++ JAVASCRIPT/NextPage.js
@@ -0,0 +1,29 @@
+window.addEventListener("keydown", ArrowKeys, false); 
+nextPage = document.getElementById('Link');
+var pages = ["thesis.html","aspiration.html","journey.html","application.html",+	
+			 "information.html","repetoire.html","../index.html"];
+var x = undefined;
+var z = window.location.href;
+console.log(z);
+
+	if (z == "http://www.tajairi.com/HTML/aspiration.html") {
+		x = 2;
+	}
+	else if(z =="http://www.tajairi.com/HTML/journey.html") {
+		x = 3;
+	}
+	else if(z =="http://www.tajairi.com/HTML/application.html") {
+		x = 4;
+	}
+	else if(z =="http://www.tajairi.com/HTML/information.html") {
+		x = 5;
+	}
+	else if(z =="http://www.tajairi.com/HTML/repetoire.html") {
+		x = 6;
+	}
+ function ArrowKeys(e) {
+   if (e.keyCode == "39") {
+   	window.location = pages[x];
+    console.log(window.location);
+   }
+}
