d84b78b8d513a7bbe0fdaf6e9ca66468c1707fed
Tajairi Neuson <tajairineuson@gmail.com>
2017-02-23 15:02:45 -0800

    Fixed NextPage.js Hopefully!

--- JAVASCRIPT/NextPage.js
+++ JAVASCRIPT/NextPage.js
@@ -1,28 +1,29 @@
 window.addEventListener("keydown", ArrowKeys, false); 
-nextPage = document.getElementById('Link');
 var pages = ["thesis.html","aspiration.html","journey.html","application.html",+	
 			 "information.html","repetoire.html","../index.html"];
-var x;
+var x = 0;
 var z = window.location.href;
 console.log(z);
-
-	if (z == "http://www.tajairi.com/HTML/aspiration.html") {
-		x = 2;
-	}
-	else if(z =="http://www.tajairi.com/HTML/journey.html") {
-		x = 3;
-	}
-	else if(z =="http://www.tajairi.com/HTML/application.html") {
-		x = 4;
-	}
-	else if(z =="http://www.tajairi.com/HTML/information.html") {
-		x = 5;
-	}
-	else if(z =="http://www.tajairi.com/HTML/repetoire.html") {
-		x = 6;
+	function CheckUrl() {
+		if (z == "http://www.tajairi.com/HTML/aspiration.html") {
+			x = 2;
+		}
+		else if(z =="http://www.tajairi.com/HTML/journey.html") {
+			x = 3;
+		}
+		else if(z =="http://www.tajairi.com/HTML/application.html") {
+			x = 4;
+		}
+		else if(z =="http://www.tajairi.com/HTML/information.html") {
+			x = 5;
+		}
+		else if(z =="http://www.tajairi.com/HTML/repetoire.html") {
+			x = 6;
+		}
 	}
  function ArrowKeys(e) {
    if (e.keyCode == "39") {
+   	CheckUrl();
    	window.location = pages[x];
     console.log(window.location);
    }
