58a9e45d661ce0409fe12960b4a8ed9590839193
Tajairi Neuson <tajairineuson@gmail.com>
2017-02-26 00:23:56 -0800

    Tajairi 0.5
    Steamrolling towards completion,
    All Pages except for Repetoire are now responsive on Desktop/Laptop
    Changed Span to H1 on Index.html
    Testing Mobile layout

---
+++ JAVASCRIPT/LoadCss.js
@@ -0,0 +1,7 @@
+if (screen.height < screen.height) {
+		console.log("Mobile Detected");
+		var css  = document.createElement('css');
+		css.rel = "stylesheet";
+		css.type = "text/css";
+		css.href = "../CSS/MOBILE/mainM.css";
+} 
