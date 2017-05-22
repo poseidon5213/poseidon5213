9f1846da7ec679ecc77bfad99b81c717ee16d564
Tajairi Neuson <tajairineuson@gmail.com>
2017-05-21 17:43:14 -0700

    Testing Repetoire Page

--- JAVASCRIPT/MusicalBars.js
+++ JAVASCRIPT/MusicalBars.js
@@ -1,22 +1,28 @@
-
-function play() {
+	var Spotify = document.getElementById('Spotify');
+	var PageName = document.getElementById('PageName');
+	var tri =  document.getElementById('tri');
+	/*
 	var JProgress = document.getElementById('JProgress');
 	var JSProgress = document.getElementById('JSProgress');
 	var HCProgress = document.getElementById('HCProgress');
 	var IProgress = document.getElementById('IProgress');
-	var PhProgress = document.getElementById('PhProgress');
-	var tri =  document.getElementById('tri');
+	var PhProgress = document.getElementById('PhProgress')
+	*/
+function progressbarchanger() {
 	tri.className = "triangle TriangleAnimation";
-	var y = 0;
-	//Can Make it even more random with an array
-	
-	while (y < 11) {
-	JProgress.value = Math.floor(Math.random() * 100);
-	/* JSProgress.value = Math.floor(Math.random() * 100);
+	Spotify.style.display = "block";
+	PageName.style.marginTop  = "5.6%";
+	tri.setAttribute( "onClick", "progressbarchangerstop()" );
+	/*JProgress.value = Math.floor(Math.random() * 100);
+	JSProgress.value = Math.floor(Math.random() * 100);
 	HCProgress.value = Math.floor(Math.random() * 100);
 	IProgress.value = Math.floor(Math.random() * 100);
-	PhProgress.value = Math.floor(Math.random() * 100); */
-	console.log("Y = " + y + " JavaProgress.value= " + JProgress.value);
-	y++;
+	PhProgress.value = Math.floor(Math.random() * 100);
+	*/
 	}
+function progressbarchangerstop() {
+	tri.className = "triangle";
+	Spotify.style.display = "none";
+	PageName.style.marginTop = "27%";
+	tri.setAttribute( "onClick", "progressbarchanger()" );
 }
